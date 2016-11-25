f0aa52b4f32f35ebc2216c87a54302324f091a9a
Gökay Gürcan <gokaygurcan@gmail.com>
2016-11-26 02:17:01 +0300

    feature: Add standard Express middlewares

--- index.js
+++ index.js
@@ -6,8 +6,13 @@
 const path = require('path');
 
 // node packages
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // local packages
 const routes = require('./routes');
@@ -16,11 +21,39 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// application settings
+app.set('trust proxy', 1);
+
+// compress response
+app.use(compression());
+
 // favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // static folder
 app.use(express.static('./public'));
+
+// process requests
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// parse cookies
+app.use(cookieParser());
+
+// simple session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'eksisozluk',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production'
+  }
+}));
+
+// simple flash messages
+app.use(flash());
 
 // routes
 app.use('/', routes);
